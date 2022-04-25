5f1830bc9fdc38965c345fb5b71f546a1112bd62
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-04-25 04:17:16 +0300

    define a method for adding users to our user_list

--- user.py
+++ user.py
@@ -1,12 +1,17 @@
 class User:
-    '''Class that generates new instances of users'''
-    user_list= [] # Empty user list
+    """Class that generates new instances of users"""
 
+    user_list = []  # Empty user list
 
     def __init__(self, username, password):
-        '''
+        """
         __init__ method to help us define properties for our user
         Args:
             username: new user username.
             password: new user password.
+        """
+    def save_user(self):
         '''
+        method that saves user objects into user_list
+        '''
+        User.user_list.append(self)
